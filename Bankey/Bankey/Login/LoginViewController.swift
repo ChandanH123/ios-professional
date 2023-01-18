4c6de64864e1838cce4d15c3f23d97ef24de9e1a
Chandan <chandanhayaran@gmail.com>
2023-01-18 21:36:30 +0530

    feature: shake signing button if login fails using Core Animation (CA).

--- Bankey/Bankey/Login/LoginViewController.swift
+++ Bankey/Bankey/Login/LoginViewController.swift
@@ -187,12 +187,12 @@ extension LoginViewController {
             return
         }
         
-//        if username.isEmpty || password.isEmpty {
-//            configureView(withMessage: "Username & password cannot be blank!")
-//            return
-//        }
+        if username.isEmpty || password.isEmpty {
+            configureView(withMessage: "Username & password cannot be blank!")
+            return
+        }
         
-        if username == "" && password == "" {
+        if username == "ch" && password == "123" {
             signInButton.configuration?.showsActivityIndicator = true
             delegate?.didLogin()
         }
@@ -204,7 +204,19 @@ extension LoginViewController {
     private func configureView(withMessage message: String) {
         errorMessageLabel.isHidden = false
         errorMessageLabel.text = message
+        shakeButton()
         
+    }
+    
+    private func shakeButton() {
+        let animation = CAKeyframeAnimation()
+        animation.keyPath = "position.x"
+        animation.values = [0, 10, -10, 10, 0] // [center, right, left, right, center] - x positions.
+        animation.keyTimes = [0, 0.16, 0.5, 0.83, 1] // key frame timings.
+        animation.duration = 0.4
+        
+        animation.isAdditive = true
+        signInButton.layer.add(animation, forKey: "shake")
     }
 }
 
